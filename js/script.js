70061db88dfb89d543128171f38e840c3435e095
LucasJoseCosta <lucjoc.1@hotmail.com>
2022-11-22 16:44:50 -0300

    Finalizado Projeto
    Projeto funcionando em dispositivos lg e xlg

---
+++ js/script.js
@@ -0,0 +1,69 @@
+$("#button-mais").click(function(){
+    $("#button-mais").addClass("d-none");
+    $("#faq-box-1").removeClass("faq-gray-box");
+    $("#faq-box-1").addClass("faq-answer-box-white");
+    $("#button-menos").removeClass("d-none");
+});
+
+$("#button-menos").click(function(){
+    $("#button-menos").addClass("d-none");
+    $("#faq-box-1").removeClass("faq-answer-box-white");
+    $("#faq-box-1").addClass("faq-gray-box");
+    $("#button-mais").removeClass("d-none");
+});
+
+$("#button-mais-2").click(function(){
+    $("#button-mais-2").addClass("d-none");
+    $("#faq-box-2").removeClass("faq-gray-box");
+    $("#faq-box-2").addClass("faq-answer-box-white");
+    $("#button-menos-2").removeClass("d-none");
+});
+
+$("#button-menos-2").click(function(){
+    $("#button-menos-2").addClass("d-none");
+    $("#faq-box-2").removeClass("faq-answer-box-white");
+    $("#faq-box-2").addClass("faq-gray-box");
+    $("#button-mais-2").removeClass("d-none");
+});
+
+$("#button-mais-3").click(function(){
+    $("#button-mais-3").addClass("d-none");
+    $("#faq-box-3").removeClass("faq-gray-box");
+    $("#faq-box-3").addClass("faq-answer-box-white");
+    $("#button-menos-3").removeClass("d-none");
+});
+
+$("#button-menos-3").click(function(){
+    $("#button-menos-3").addClass("d-none");
+    $("#faq-box-3").removeClass("faq-answer-box-white");
+    $("#faq-box-3").addClass("faq-gray-box");
+    $("#button-mais-3").removeClass("d-none");
+});
+
+$("#button-mais-4").click(function(){
+    $("#button-mais-4").addClass("d-none");
+    $("#faq-box-4").removeClass("faq-gray-box");
+    $("#faq-box-4").addClass("faq-answer-box-white");
+    $("#button-menos-4").removeClass("d-none");
+});
+
+$("#button-menos-4").click(function(){
+    $("#button-menos-4").addClass("d-none");
+    $("#faq-box-4").removeClass("faq-answer-box-white");
+    $("#faq-box-4").addClass("faq-gray-box");
+    $("#button-mais-4").removeClass("d-none");
+});
+
+$("#button-mais-5").click(function(){
+    $("#button-mais-5").addClass("d-none");
+    $("#faq-box-5").removeClass("faq-gray-box");
+    $("#faq-box-5").addClass("faq-answer-box-white");
+    $("#button-menos-5").removeClass("d-none");
+});
+
+$("#button-menos-5").click(function(){
+    $("#button-menos-5").addClass("d-none");
+    $("#faq-box-5").removeClass("faq-answer-box-white");
+    $("#faq-box-5").addClass("faq-gray-box");
+    $("#button-mais-5").removeClass("d-none");
+});
